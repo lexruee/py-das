aa7efdcee9f7c8c47de646dc1abb7770e5ec3070
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2017-11-21 03:10:14 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Alexander Rüedlinger"
+
+from setuptools import setup, find_packages
+
+setup(
+    name='das',
+    version='0.1',
+    packages=find_packages(),
+)
