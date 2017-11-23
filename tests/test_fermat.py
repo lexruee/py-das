76f81c9f4ae1de28c98506f97e131cb72284ff96
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2017-11-23 05:59:05 +0100

    add algo to create fermat numbers

---
+++ tests/test_fermat.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+
+from das.algo import fermat
+
+
+NUMBERS = [3, 5, 17, 257, 65537]
+
+
+def test_fermat_number():
+    for i, fnum in zip(range(0, len(NUMBERS)), NUMBERS):
+        assert fermat.fermat_number(i) == fnum
+
+def test_fermat_numbers():
+    for fn in fermat.fermat_numbers(5)
+        assert fn in NUMBERS
