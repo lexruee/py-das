ba53a190dc08e756a481b280969d3120dad4bfd9
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2018-07-04 19:44:10 +0200

    update sort module
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

--- tests/test_sort.py
+++ tests/test_sort.py
@@ -4,25 +4,38 @@ from das.algo import sort
 
 
 EXAMPLES = [
-    ([3,4,1,2,6,5], [1,2,3,4,5,6])
+    ([3, 4, 1, 2, 6, 5], [1, 2, 3, 4, 5, 6]),
+    ([9, 3, 2, 6, 2, 1], [1, 2, 2, 3, 6, 9])
 ]
 
 
-def get_example(n):
-    ain, aout = EXAMPLES[n][0], EXAMPLES[n][1]
-    return ain[:], aout[:]
-
 def test_insertion_sort():
-    ain, aout = get_example(0) 
-    assert ain != aout
-    sort.insertion_sort(ain)
-    assert ain == aout
+    for ain, aout in EXAMPLES:
+        _ain = ain[:]
+        assert _ain != aout
+        sort.insertion_sort(_ain)
+        assert _ain == aout
+
 
 def test_selection_sort():
-    ain, aout = get_example(0) 
-    assert ain != aout
-    sort.insertion_sort(ain)
-    assert ain == aout
+    for ain, aout in EXAMPLES:
+        _ain = ain[:]
+        assert _ain != aout
+        sort.selection_sort(_ain)
+        assert _ain == aout
+
+
+def test_quick_sort():
+    for ain, aout in EXAMPLES:
+        _ain = ain[:]
+        assert _ain != aout
+        sort.quick_sort(_ain)
+        assert _ain == aout
+
 
 def test_merge_sort():
-    pass
+    for ain, aout in EXAMPLES:
+        _ain = ain[:]
+        assert _ain != aout
+        r = sort.merge_sort(_ain)
+        assert r == aout
